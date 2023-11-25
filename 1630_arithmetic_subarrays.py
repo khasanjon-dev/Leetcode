59bf17236dfcb9d157c21569fffb96fb1abbea23
khasanjon <khasanjon.dev@gmail.com>
2023-11-25 11:04:22 +0500

    update file

--- 1630_arithmetic_subarrays.py
+++ 1630_arithmetic_subarrays.py
@@ -1,20 +1,3 @@
-def check_arithmetic_sub_arrays(nums: list[int], l: list[int], r: list[int]):
-    result = []
-    for j in range(len(l)):
-        nums_cut = nums[l[j]: r[j] + 1]
-        res = abs(abs(nums_cut[1]) - abs(nums_cut[0]))
-        for k in range(len(nums_cut)):
-            for l in range(1, len(nums_cut)):
-                if res != abs(abs(nums_cut[l]) - abs(nums_cut[k])):
-                    result.append(False)
-                    break
-        else:
-            result.append(True)
-    return result
+from main import print_string
 
-
-nums = [-3, -6, -8, -4, -2, -8, -6, 0, 0, 0, 0]
-l = [5, 4, 3, 2, 4, 7, 6, 1, 7]
-r = [6, 5, 6, 3, 7, 10, 7, 4, 10]
-# Output: [true,false,true]
-print(check_arithmetic_sub_arrays(nums, l, r))
+print_string()
--- main.py
+++ main.py
@@ -1,77 +1,12 @@
-# x = input()
-my_dict = {
-    '1': 'bir',
+def is_palindrome(s: str) -> bool:
+    result = ''
+    for letter in s:
+        if 48 <= ord(letter) <= 57 or 65 <= ord(letter) <= 90 or 97 <= ord(letter) <= 122:
+            if 65 <= ord(letter) <= ord(letter) <= 90:
+                result += chr(ord(letter) + 32)
+            else:
+                result += letter
+    return result == result[::-1]
 
-    '2': 'ikki',
 
-    '3': 'uch',
-
-    '4': 'to’rt',
-
-    '5': 'besh',
-
-    '6': 'olti',
-
-    '7': 'yetti',
-
-    '8': 'sakkiz',
-
-    '9': 'to’qqiz',
-    '10': 'o’n',
-
-    '20': 'yigirma',
-
-    '30': 'o’ttiz',
-
-    '40': 'qirq',
-
-    '50': 'ellik',
-
-    '60': 'oltmish',
-
-    '70': 'yetmish',
-
-    '80': 'sakson',
-
-    '90': 'to’qson',
-
-    '100': 'yuz',
-
-    '1000': 'ming',
-
-}
-print(my_dict)
-# result = ''
-# if len(x) >= 4:
-#     if x[1] == '0' and x[2] == '0' and x[3] == '0':
-#         result = my_dict[x] if x[0] == 1 else my_dict[x[0]] + ' ' + my_dict['1000']
-#     elif x[1] == '0' and x[2] == '0':
-#         result = my_dict[x[0]] + ' ' + my_dict['1000'] + ' ' + my_dict[x[3]]
-#     elif x[2] == '0' and x[3] == '0':
-#         result = my_dict[x[0]] + ' ' + my_dict['1000'] + ' ' + my_dict[x[1]] + ' ' + my_dict['100']
-#     elif x[1] == '0' and x[3] == '0':
-#         result = my_dict[x[0]] + ' ' + my_dict['1000'] + ' ' + my_dict[x[2] + '0']
-#     elif x[1] == '0':
-#         result = my_dict[x[0]] + ' ' + my_dict['1000'] + ' ' + my_dict[x[2] + '0'] + ' ' + my_dict[x[3]]
-#     elif x[2] == '0':
-#         result = my_dict[x[0]] + ' ' + my_dict['1000'] + ' ' + my_dict[x[1]] + ' ' + my_dict['100'] + ' ' + my_dict[
-#             x[3]]
-#     elif x[3] == '0':
-#         result = my_dict[x[0]] + ' ' + my_dict['1000'] + ' ' + my_dict[x[1]] + ' ' + my_dict['100'] + ' ' + my_dict[
-#             x[2] + '0']
-#     else:
-#         result = my_dict[x[0]] + ' ' + my_dict['1000'] + ' ' + my_dict[x[1]] + ' ' + my_dict['100'] + ' ' + my_dict[
-#             x[2] + '0'] + ' ' + my_dict[x[3]]
-# elif len(x) >= 3:
-#     if x[1] == '0' and x[2] == '0':
-#         result = my_dict[x] if x[0] == 1 else my_dict[x[0]] + my_dict['100']
-#     elif x[-1] == '0':
-#         result = my_dict[x[0]] + ' ' + my_dict['100'] + ' ' + my_dict[x[1] + '0']
-#     else:
-#         result = my_dict[x[0]] + ' ' + my_dict['100'] + ' ' + my_dict[x[2]] if x[1] == '0' else my_dict[x[
-#             0]] + ' ' + my_dict['100'] + ' ' + my_dict[x[1] + '0'] + ' ' + my_dict[x[2]]
-# elif len(x) >= 2:
-#     result = my_dict[x] if x[-1] == '0' else my_dict[x[0] + '0'] + ' ' + my_dict[x[1]]
-# else:
-#     result = my_dict[x]
-# print(result)
+print(is_palindrome("A man, a plan, a canal: Panama"))
