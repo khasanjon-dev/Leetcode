5ad33f08e5897fd546d25930b6ee2371486ddb73
khasanjon <khasanjon.dev@gmail.com>
2023-12-02 08:13:08 +0500

    update file

--- lesson_1.py
+++ lesson_1.py
@@ -1,12 +1,1 @@
-class Solution:
-    def maximum_wealth(self, nums: list[list[int]]) -> int:
-        # result = []
-        # for num in nums:  # [1, 2, 3]
-        #     result.append(sum(num))
-        # return max(result)
-        return max([sum(num) for num in nums])
 
-
-solution = Solution()
-result = solution.maximum_wealth([[1, 2, 3], [3, 2, 1]])
-print(result)
