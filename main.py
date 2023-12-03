60d79366ba4f26072703b8acd1731b3da0c3a7ea
khasanjon <khasanjon.dev@gmail.com>
2023-12-03 13:38:11 +0500

    update file

--- main.py
+++ main.py
@@ -1,31 +1,3 @@
-# latitude = 41.283767
-# longitude = 69.210916
-#
-# import googlemaps
-#
-# gmaps = googlemaps.Client(key=key)
-# reverse_geocode_result = gmaps.reverse_geocode((latitude, longitude))
-# if reverse_geocode_result:
-#     for result in reverse_geocode_result:
-#         print("Formatted Address:", result['formatted_address'])
-#
-#         for component in result['address_components']:
-#             print(f"{component['types'][0].capitalize()}: {component['long_name']}")
-#
-#             # Check for street number and name
-#             if 'street_number' in component['types']:
-#                 street_number = component['long_name']
-#                 print(f"Street Number: {street_number}")
-#             elif 'route' in component['types']:
-#                 street_name = component['long_name']
-#                 print(f"Street Name: {street_name}")
-#
-#         print("Place ID:", result['place_id'])
-#         print("Types:", result['types'])
-#         print("\n---\n")
-# else:
-#     print("No results found.")
-
 from geopy.geocoders import Nominatim
 
 latitude = 41.283493
