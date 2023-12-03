6ac7b100a03eaaf4aae22f2ef25be3764792f38c
khasanjon <khasanjon.dev@gmail.com>
2023-12-03 14:57:17 +0500

    update file

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from geopy.geocoders import Nominatim
 
 latitude = 41.283493
 longitude = 69.210893
-geolocator = Nominatim(user_agent="oqtepa")
-location = geolocator.reverse(f"{latitude}, {longitude}")
-print(location.address)
-print((location.latitude, location.longitude))
-print(location.raw['address'])
-print()
+
+
+def get_address(latitude: float, longitude: float):
+    geolocator = Nominatim(user_agent="oqtepa")
+    location = geolocator.reverse(f"{latitude}, {longitude}")
+    address = location.raw['address']
+    house_number = address.get('house_number', None)
+    road = address.get('road', None)
+    region = address.get('county', None)
+    city = address['city']
+    return address
+
+
+print(get_address(latitude, longitude))
