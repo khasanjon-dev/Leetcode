5841ad154b11d7df2e52e8839c404562f96114f1
khasanjon <khasanjon.dev@gmail.com>
2023-12-05 10:28:46 +0500

    update file

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from itertools import combinations
+from itertools import combinations, chain
 
 
 def sum_counts(nums: list[int]) -> int:
-    result = []
-    for i in range(1, len(nums) + 1):
-        tup = tuple(combinations(nums, i))
-        res = []
-        for t in tup:
-            res.extend(set(t))
-        result.append(res)
+    res = []
+    for i in range(2, len(nums) + 1):
+        l = list(combinations(nums, i))
+        r = []
+        for t in l:
+            if len(set(t)) == 1:
+                pass
+            else:
+                r.extend(t)
+        res.append(r)
+    res.append(nums)
+    result = sum([num ** 2 for num in chain(*res)])
 
-
-    return result
+    print(result)
 
 
 print(sum_counts([1, 2, 1]))
