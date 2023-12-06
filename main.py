e8cc314b9aefcd7618a75e84dd1f5b57f15ce5b7
khasanjon <khasanjon.dev@gmail.com>
2023-12-06 15:27:36 +0500

    update some files

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-def merger_the_tools(string: str, k: int):
-    for i in range(0, len(string), k):
-        s = string[i: i + k]
-        if len(set(s)) == len(s):
-            print(s)
-        else:
-            unique_s = ''
-            for j in s:
-                if j not in unique_s:
-                    unique_s += j
-            print(unique_s)
+def is_acronym(words: list[str], s: str) -> bool:
+    res = ''
+    for word in words:
+        res += word[0]
+    return res == s
 
 
-string = 'AABCAAADA'
-merger_the_tools(string, 3)
+print(is_acronym(['alice', 'bob', 'charlie'], 'abc'))
