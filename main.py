41565288d833343205847c3b8779a5be3f3ff6a3
khasanjon <khasanjon.dev@gmail.com>
2023-12-06 19:26:56 +0500

    2710. Remove Trailing Zeros From a String

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-from string import ascii_lowercase as lower
+def fun(num: str) -> str:
+    return str(int(num[::-1]))[::-1]
 
-
-def fun(s: str):
-    result = []
-    for i, w in enumerate(s):
-        if w == '#':
-            result.pop()
-            result.pop()
-            result.append(lower[int(s[i - 2: i]) - 1])
-        else:
-            result.append(lower[int(s[i]) - 1])
-    res = ''
-    for w in result:
-        res += w
-    return res
-
-
-print(fun('10#11#12'))
+print(fun('123'))
