296cf21ed6c7bbb143ab673b362eb49094cc43e3
khasanjon <khasanjon.dev@gmail.com>
2023-12-07 23:07:00 +0500

    1941 Check if All Characters Have Equal Number of Occurrences

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-def fun(coordinates: str) -> bool:
-    data = {
-        'a': [False, True],
-        'b': [True, False],
-        'c': [False, True],
-        'd': [True, False],
-        'e': [False, True],
-        'f': [True, False],
-        'g': [False, True],
-        'h': [True, False]
-    }
-    if int(coordinates[-1]) % 2:
-        return data[coordinates[0]][0]
-    else:
-        return data[coordinates[0]][1]
+from collections import Counter
 
 
-print(fun('b1'))
+def fun(s: str) -> bool:
+    counter = Counter(s)
+    num = counter[s[0]]
+    for value in counter.values():
+        if not num == value:
+            return False
+    return True
+
+
+print(fun("abacbc"))
