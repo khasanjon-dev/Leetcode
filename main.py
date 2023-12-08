8aa30b2e51321f63fe23a36431aba2d0676b2002
khasanjon <khasanjon.dev@gmail.com>
2023-12-08 23:24:45 +0500

    2678. Number of Senior Citizens

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-from collections import Counter
+def fun(details: list[str]) -> int:
+    result = 0
+    for detail in details:
+        res = detail[11:-2]
+        if int(res) > 60:
+            result += 1
+    return result
 
 
-def fun(s: str) -> bool:
-    counter = Counter(s)
-    num = counter[s[0]]
-    for value in counter.values():
-        if not num == value:
-            return False
-    return True
-
-
-print(fun("abacbc"))
+print(fun(["5612624052M0130", "5378802576M6424", "5447619845F0171", "2941701174O9078"]))
