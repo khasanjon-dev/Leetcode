5aa70456944a37b64f510b04ea8fb676f76c2c59
khasanjon <khasanjon.dev@gmail.com>
2023-12-08 23:45:59 +0500

    657. Robot Return to Origin

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def fun(details: list[str]) -> int:
-    result = 0
-    for detail in details:
-        res = detail[11:-2]
-        if int(res) > 60:
-            result += 1
-    return result
+def fun(moves: str) -> bool:
+    return moves.count('D') == moves.count('U') and moves.count('L') == moves.count('R')
 
 
-print(fun(["5612624052M0130", "5378802576M6424", "5447619845F0171", "2941701174O9078"]))
+print(fun("LDLLULUDLL"))
