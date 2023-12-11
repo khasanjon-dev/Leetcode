c0e5078742e2d999299de65a49accb4446b34bca
khasanjon <khasanjon.dev@gmail.com>
2023-12-11 18:57:14 +0500

    delete some files

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def fun(moves: str) -> bool:
-    return moves.count('D') == moves.count('U') and moves.count('L') == moves.count('R')
+text = '1a2b3c4d5a6b7b8d9a10b11c12b13b14a15b16b17c18b19a20b21a22b23c24b25b26b27a28c29a30b'
 
 
-print(fun("LDLLULUDLL"))
+def keys_serializer(text: str):
+    if text.isalnum():
+        res = ''
+        for letter in text:
+            if letter.isalpha():
+                res += letter
+        return res
+    return text
+
+
+print(keys_serializer(text))
