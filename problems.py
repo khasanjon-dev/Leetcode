8651ce19272e4d3bb32208512d17167b00266a40
khasanjon <khasanjon.dev@gmail.com>
2023-12-17 22:59:04 +0500

    update some files

--- problems.py
+++ problems.py
@@ -1,10 +1,9 @@
-def fun(words: list[str], left: int, right: int) -> int:
-    vowels = 'aeiou'
+from string import ascii_lowercase as lower
+
+
+def fun(word: str) -> int:
     result = 0
-    for word in words[left: right + 1]:
-        if word[0] in vowels and word[-1] in vowels:
-            result += 1
-    return result
-
-
-print(fun(['hey', 'aeo', 'mu', 'ooo', 'artro'], 1, 4))
+    re_lower = lower[::-1]
+    for w in word:
+        left = lowe
+print(fun('abc'))
