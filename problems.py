84ca4c92d4cbaf9669983eca5662b25d84363b76
Khasanjon Mamadaliyev <khasanjon.dev@gmail.com>
2023-12-25 11:48:27 +0500

    2824. Count Pairs Whose Sum is Less than Target

--- problems.py
+++ problems.py
@@ -1,8 +1,11 @@
-def number_of_employees_who_met_target(hours: list[int], target: int) -> int:
-    count = 0
-    for hour in hours:
-        if hour >= target:
-            count += 1
-    return count
+def count_pairs(nums: list[int], target: int) -> int:
+    result = 0
+    len_nums = len(nums)
+    for i in range(len_nums):
+        for j in range(i + 1, len_nums):
+            if nums[i] + nums[j] < target:
+                result += 1
+    return result
 
 
+print(count_pairs([-6, 2, 5, -2, -7, -1, 3], -2))
