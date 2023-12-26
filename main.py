011a463477f8b3252bb92260221661f6d989cf92
Khasanjon Mamadaliyev <khasanjon.dev@gmail.com>
2023-12-26 23:44:44 +0500

    update some files

--- main.py
+++ main.py
@@ -1,64 +1,24 @@
-import httpx
+import os
+from pprint import pprint
 
+import sib_api_v3_sdk
+from dotenv import load_dotenv
+from sib_api_v3_sdk.rest import ApiException
 
-def keys_serializer(text: str):
-    if text.isalnum():
-        res = ''
-        for letter in text:
-            if letter.isalpha():
-                res += letter
-        return res
-    return text
+load_dotenv()
 
+BREVO_KEY = os.getenv('BREVO_API_KEY')
 
-#
-# res = keys_serializer('1a2b3c4d5a6b7b8d9a10b11c12b13b14a15b16b17c18b19a20b21a22b23c24b25b26b27a28c29a30b')
-# print(len(res))
-# from datetime import datetime
-#
-# import httpx
-#
-# url = "http://127.0.0.1:8000/api/science/"
-# context = {
-#     'name': 'test_name',
-#     'size': 20,
-#     'keys': 'test_keys',
-#     'author': 1
-# }
-# # http://127.0.0.1:8000/api/science/
-# response = httpx.post(url, data=context)
-# date = response.json()['created_at']
-# datetime_object = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f%z")
-#
-# # Format the datetime object using strftime
-# formatted_time = datetime_object.strftime("%m/%d/%Y, %H:%M:%S")
-# print(formatted_time)
-
-
-
-# def get_test_id(text: str) -> int:
-#     res = ''
-#     for w in text:
-#         if w.isnumeric():
-#             res += w
-#     return int(res)
-
-base_url = 'http://127.0.0.1:8000/api'
-
-
-def check_answer(keys: str):
-    tru_answers = 0
-    false_answers = 0
-    url = f'{base_url}/science/18/'
-    keys = keys_serializer(keys)
-    response = httpx.get(url)
-    keys_api = keys_serializer(response.json()['keys'])
-    for k1, k2 in zip(keys, keys_api):
-        if k1 == k2:
-            tru_answers += 1
-        else:
-            false_answers += 1
-    return tru_answers, false_answers
-
-
-print(check_answer('1a2b3c4d5a6b7b8d9a10b11c12b13b14a15b16b17c18b19a20b21a22b23c24b25b26b27a28c29b30a'))
+sib_api_v3_sdk.configuration.api_key['api-key'] = BREVO_KEY
+api_instance = sib_api_v3_sdk.EmailCampaignsApi()
+email_campaigns = sib_api_v3_sdk.CreateEmailCampaign(
+    name="Campaign sent via the API",
+    subject="My subject",
+    sender={"name": "From name", "email": "myfromemail@mycompany.com"}, type="classic",
+    html_content="Congratulations! You successfully sent this example campaign via the Brevo API.",
+    recipients={"listIds": [2, 7]})
+try:
+    api_response = api_instance.create_email_campaign(email_campaigns)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling EmailCampaignsApi->create_email_campaign: %s\n" % e)
