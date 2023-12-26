647e786480008fe542adb740c1b5ffd9c113ba68
Khasanjon Mamadaliyev <khasanjon.dev@gmail.com>
2023-12-26 23:07:52 +0500

    2733. Neither Minimum nor Maximum

--- problems.py
+++ problems.py
@@ -1,13 +1,9 @@
-from itertools import combinations
-
-
-def minimum_sum(num: int) -> int:
-    nums = [int(n) for n in str(num)]
+def find_non_min_or_max(nums: list[int]) -> int:
+    nums = list(set(nums))
+    if len(nums) <= 2:
+        return -1
     nums.sort()
-    num1, num2 = nums[0], nums[1]
-    num3, num4 = nums[2], nums[3]
-    new_1 = int(str(min(num1, num2)) + str(max(num3, num4)))
-    new_2 = int(str(max(num1, num2)) + str(min(num3, num4)))
-    return new_1 + new_2
+    return nums[1]
 
-print(minimum_sum(2932))
+
+print(find_non_min_or_max([3, 2, 1, 4]))
