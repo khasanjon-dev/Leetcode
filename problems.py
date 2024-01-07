9866556867783cab0d153b6d1656e25192bf0344
Khasanjon <khasanjon.dev@gmail.com>
2024-01-07 12:38:51 +0500

    2859. Sum of Values at Indices With K Set Bits

--- problems.py
+++ problems.py
@@ -1,18 +1,5 @@
-def find_relative_ranks(score: list[int]) -> list[str]:
-    score_sort = sorted(score, reverse=True)
-    result = []
-    data = {
-        1: 'Gold Medal',
-        2: 'Silver Medal',
-        3: 'Bronze Medal'
-    }
-    for num in score:
-        index = score_sort.index(num)  + 1
-        if index in data.keys():
-            result.append(data[index])
-        else:
-            result.append(str(index))
-    return result
+def sum_indices_with_k_set_bits(nums: list[int], k: int) -> int:
+    return sum([nums[i] for i in range(len(nums)) if k == bin(i).count('1')])
 
 
-find_relative_ranks([10, 3, 8, 9, 4])
+print(sum_indices_with_k_set_bits([5, 10, 1, 5, 2], 1))
