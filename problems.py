8e4aae8fc9b7e4c0ebc8715d6d17ab1844d28e9a
Khasanjon <khasanjon.dev@gmail.com>
2024-01-08 11:17:05 +0500

    2697. Lexicographically Smallest Palindrome

--- problems.py
+++ problems.py
@@ -1,5 +1,27 @@
-def sum_indices_with_k_set_bits(nums: list[int], k: int) -> int:
-    return sum([nums[i] for i in range(len(nums)) if k == bin(i).count('1')])
+def make_smallest_palindrome(s: str) -> str:
+    result = ''
+    l = len(s)
+    a = s[: l // 2]
+    c = None
+    if l % 2:
+        b = s[l // 2 + 1:]
+        c = s[l // 2]
+    else:
+        b = s[l // 2:]
+    res = ''
+    for i in range(1, len(a) + 1):
+        if a[i - 1] != b[-i]:
+            if a[i - 1] > b[-i]:
+                res += b[-i]
+            else:
+                res += a[i - 1]
+        else:
+            res += a[i - 1]
+    if c:
+        result += res + c + res[::-1]
+    else:
+        result += res + res[::-1]
+    return result
 
 
-print(sum_indices_with_k_set_bits([5, 10, 1, 5, 2], 1))
+print(make_smallest_palindrome('egcfe'))
