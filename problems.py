c016e2af908a0cf5b5c63d2f67e922a566c92f4a
Khasanjon <khasanjon.dev@gmail.com>
2024-01-10 08:40:49 +0500

    338. Counting Bits

--- problems.py
+++ problems.py
@@ -1,15 +1,7 @@
-def arithmetic_triplets(nums: list[int], diff: int) -> int:
-    l = len(nums)
-    result = 0
-    for i in range(l):
-        num = nums[i]
-        for j in range(i + 1, l):
-            if diff == nums[j] - num:
-                for k in range(j + 1, l):
-                    if diff == nums[k] - nums[j]:
-                        result += 1
-                        break
-    return result
+def count_bits(n: int) -> list[int]:
+    return [bin(num)[2:].count('1') for num in range(n +1)]
 
 
-print(arithmetic_triplets([4, 5, 6, 7, 8, 9], 2))
+
+
+print(count_bits(5))
