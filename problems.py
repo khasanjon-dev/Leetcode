5a77664ddaed3e118363400150607bd8046d9baa
Khasanjon <khasanjon.dev@gmail.com>
2024-01-10 08:41:20 +0500

    338. Counting Bits

--- problems.py
+++ problems.py
@@ -1,7 +1,5 @@
 def count_bits(n: int) -> list[int]:
-    return [bin(num)[2:].count('1') for num in range(n +1)]
-
-
+    return [bin(num)[2:].count('1') for num in range(n + 1)]
 
 
 print(count_bits(5))
