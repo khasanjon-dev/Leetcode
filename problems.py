4076e24a6255d6fba0570be349ea9d37a8f5f101
Khasanjon <khasanjon.dev@gmail.com>
2024-01-13 10:19:03 +0500

    1876. Substrings of Size Three with Distinct Characters

--- problems.py
+++ problems.py
@@ -1,13 +1,9 @@
-def delete_greatest_value(grid: list[list[int]]) -> int:
-    n = len(grid[0])
+def count_good_sub_strings(s: str) -> int:
     result = 0
-    for i in range(n):
-        max_ = 0
-        for row in grid:
-            i = row.index(max(row))
-            max_ = max(max_, row.pop(i))
-        result += max_
+    for i in range(len(s)):
+        if 3 == len(set(s[i: i + 3])):
+            result += 1
     return result
 
 
-print(delete_greatest_value([[1, 2, 4], [3, 3, 1]]))
+print(count_good_sub_strings('aababcabc'))
