1cc5e0755981ceb185948d4e14906d5632dc5fdb
Khasanjon <khasanjon.dev@gmail.com>
2024-01-22 12:19:58 +0500

    520. Detect Capital

--- problems.py
+++ problems.py
@@ -1,21 +1,11 @@
-def last_visited_integers(words: list[str]) -> list[int]:
-    result = []
-    nums = []
-    prev_count = 0
-    for word in words:
-        if word == 'prev':
-            if prev_count >= len(nums):
-                result.append(-1)
-            else:
-                result.append(nums[-1 - prev_count])
-            prev_count += 1
-        else:
-            nums.append(int(word))
-            result.append(int(word))
-            prev_count = 0
-
-    return result
+def detect_capital_use(word: str) -> bool:
+    if word.isupper():
+        return True
+    elif word == word.capitalize():
+        return True
+    elif word.islower():
+        return True
+    return False
 
 
-words = ["1", "prev", "2", "prev", "prev", "3", "4", "prev", "5"]
-print(last_visited_integers(words))
+print(detect_capital_use('FlaG'))
