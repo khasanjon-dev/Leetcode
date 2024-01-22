0bdeb8249d20226bf12bf7fd2fb2fdfaa483205d
Khasanjon <khasanjon.dev@gmail.com>
2024-01-22 12:35:20 +0500

    520. Detect Capital

--- problems.py
+++ problems.py
@@ -1,9 +1,16 @@
 def detect_capital_use(word: str) -> bool:
-    if word.isupper():
+    res = 0
+    for w in word:
+        if 65 <= ord(w) <= 90:
+            res += 1
+    if res == len(word):
         return True
-    elif word == word.capitalize():
+    elif res == 1:
+        for w in word[1:]:
+            if 65 <= ord(w) <= 90:
+                return False
         return True
-    elif word.islower():
+    elif res == 0:
         return True
     return False
 
