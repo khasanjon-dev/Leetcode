652a164af7a59a3699f782b9ce265b926a8c49a9
Khasanjon <khasanjon.dev@gmail.com>
2024-01-23 09:33:24 +0500

    1408. String Matching in an Array

--- problems.py
+++ problems.py
@@ -1,18 +1,11 @@
-def detect_capital_use(word: str) -> bool:
-    res = 0
-    for w in word:
-        if 65 <= ord(w) <= 90:
-            res += 1
-    if res == len(word):
-        return True
-    elif res == 1:
-        for w in word[1:]:
-            if 65 <= ord(w) <= 90:
-                return False
-        return True
-    elif res == 0:
-        return True
-    return False
+def string_matching(words: list[str]) -> list[str]:
+    result = []
+    for i, w in enumerate(words):
+        for j, word in enumerate(words):
+            if w in word and i != j:
+                result.append(w)
+                break
+    return result
 
 
-print(detect_capital_use('FlaG'))
+print(string_matching(['leetcoder', 'leetcode', 'od', 'hamlet', 'am']))
