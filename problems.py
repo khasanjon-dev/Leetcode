0830b70e7e8d036256569d8338fc7fa50203fdcd
Khasanjon <khasanjon.dev@gmail.com>
2024-01-24 08:33:06 +0500

    1790. Check if One String Swap Can Make Strings Equal

--- problems.py
+++ problems.py
@@ -1,11 +1,14 @@
-def string_matching(words: list[str]) -> list[str]:
-    result = []
-    for i, w in enumerate(words):
-        for j, word in enumerate(words):
-            if w in word and i != j:
-                result.append(w)
-                break
-    return result
+def are_almost_equal(s1: str, s2: str) -> bool:
+    if s1 == s2:
+        return True
+    if len(s1) != len(s2):
+        return False
+    for j in range(len(s2)):
+        for i in range(1, len(s2)):
+            s3 = s2[:j] + s2[i] + s2[j + 1:i] + s2[j] + s2[i + 1:]
+            if s3 == s1:
+                return True
+    return False
 
 
-print(string_matching(['leetcoder', 'leetcode', 'od', 'hamlet', 'am']))
+print(are_almost_equal("siyolsdcjthwsiplccjbuceoxmpjgrauocx", "siyolsdcjthwsiplccpbuceoxmjjgrauocx"))
