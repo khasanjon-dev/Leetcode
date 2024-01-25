04d94b1a71aa3a450c4f0b693a7612c38494cbfe
Khasanjon <khasanjon.dev@gmail.com>
2024-01-25 23:24:18 +0500

    1805. Number of Different Integers in a String

--- problems.py
+++ problems.py
@@ -1,14 +1,16 @@
-def are_almost_equal(s1: str, s2: str) -> bool:
-    if s1 == s2:
-        return True
-    if len(s1) != len(s2):
-        return False
-    for j in range(len(s2)):
-        for i in range(1, len(s2)):
-            s3 = s2[:j] + s2[i] + s2[j + 1:i] + s2[j] + s2[i + 1:]
-            if s3 == s1:
-                return True
-    return False
+def num_different_integers(word: str) -> int:
+    result = []
+    res = ''
+    for w in word:
+        if w.isdigit():
+            res += w
+        else:
+            if res:
+                result.append(int(res))
+            res = ''
+    if res:
+        result.append(int(res))
+    return len(set(result))
 
 
-print(are_almost_equal("siyolsdcjthwsiplccjbuceoxmpjgrauocx", "siyolsdcjthwsiplccpbuceoxmjjgrauocx"))
+print(num_different_integers('a1b01c001'))
