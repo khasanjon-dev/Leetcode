a5d664dbd64f76c3f14bd967c9a5e215c6fe3b79
Khasanjon <khasanjon.dev@gmail.com>
2024-01-25 23:42:12 +0500

    Code Testcase Testcase Test Result 1507. Reformat Date

--- problems.py
+++ problems.py
@@ -1,16 +1,16 @@
-def num_different_integers(word: str) -> int:
-    result = []
-    res = ''
-    for w in word:
-        if w.isdigit():
-            res += w
-        else:
-            if res:
-                result.append(int(res))
-            res = ''
-    if res:
-        result.append(int(res))
-    return len(set(result))
+def reformat_date(date: str) -> str:
+    month = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08',
+             'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
+    result = ''
+    d, m, year = date.split()
+    result += year
+    result += '-' + month[m]
+    result += '-'
+    if len(d) == 4:
+        result += d[:2]
+    else:
+        result += '0' + d[0]
+    return result
 
 
-print(num_different_integers('a1b01c001'))
+print(reformat_date('20th Oct 2052'))
