f7db03398eee39ec6f6bfe987433c3cdaa4a8c20
Khasanjon <khasanjon.dev@gmail.com>
2024-01-29 11:37:59 +0500

    2788. Split Strings by Separator

--- problems.py
+++ problems.py
@@ -1,17 +1,20 @@
-def reformat_date(date: str) -> str:
-    month = {'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06', 'Jul': '07', 'Aug': '08',
-             'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'}
-    result = ''
-    d, m, year = date.split()
-    result += year
-    result += '-' + month[m]
-    result += '-'
-    if len(d) == 4:
-        result += d[:2]
-    else:
-        result += '0' + d[0]
+def split_word_by_seperator(words: list[str], separator: str) -> list[str]:
+    result = []
+    for word in words:
+        if separator in word:
+            res = ''
+            for w in word:
+                if w == separator:
+                    if res:
+                        result.append(res)
+                        res = ''
+                else:
+                    res += w
+            if res:
+                result.append(res)
+        else:
+            result.append(word)
     return result
 
 
-
-print(reformat_date('20th Oct 2052'))
+print(split_word_by_seperator(["$easy$", "$problem$"], "$"))
