35881afc7ec7b3a42b440a57c402f212cda85abe
Khasanjon <khasanjon.dev@gmail.com>
2024-02-03 03:38:35 +0500

    1496. Path Crossing

--- problems.py
+++ problems.py
@@ -1,20 +1,21 @@
-def split_word_by_seperator(words: list[str], separator: str) -> list[str]:
-    result = []
-    for word in words:
-        if separator in word:
-            res = ''
-            for w in word:
-                if w == separator:
-                    if res:
-                        result.append(res)
-                        res = ''
-                else:
-                    res += w
-            if res:
-                result.append(res)
-        else:
-            result.append(word)
-    return result
+def is_path_crossing(path: str) -> bool:
+    x, y = 0, 0
+    visited = {(0, 0)}
+    for move in path:
+        if move == 'N':
+            y += 1
+        elif move == 'S':
+            y -= 1
+        elif move == 'E':
+            x += 1
+        elif move == 'W':
+            x -= 1
+        if (x, y) in visited:
+            return True
+        visited.add((x, y))
+    return False
 
 
-print(split_word_by_seperator(["$easy$", "$problem$"], "$"))
+path = "NESWW"
+result = is_path_crossing(path)
+print(result)
