42612504f21baeaa264d9c7f69c1ea6f9d8e4121
khasanjon <khasanjon.dev@gmail.com>
2024-03-09 10:46:16 +0500

    2540

--- solutions/2540. Minimum Common Value/2540.py
+++ solutions/2540. Minimum Common Value/2540.py
@@ -1,3 +1,6 @@
 def get_common(nums1: list[int], nums2: list[int]) -> int:
     common_datas = set(nums1).intersection(nums2)
     return min(common_datas) if common_datas else -1
+
+
+print(get_common([1, 2, 3, 6], [2, 3, 4, 5]))
