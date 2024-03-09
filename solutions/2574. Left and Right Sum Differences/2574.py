0c5daf45d7030ad12e8862c0c3b5fd7b6c613b75
khasanjon <khasanjon.dev@gmail.com>
2024-03-09 16:00:05 +0500

    2574

--- solutions/2574. Left and Right Sum Differences/2574.py
+++ solutions/2574. Left and Right Sum Differences/2574.py
@@ -1,11 +1,5 @@
 def left_right_difference(nums: list[int]) -> list[int]:
-    result = []
-    len_nums = len(nums)
-    if len_nums > 1:
-        for i in range(len_nums):
-            result.append(abs(sum(nums[:i]) - sum(nums[i + 1:])))
-        return result
-    return [0]
+    return [abs(sum(nums[:i]) - sum(nums[i + 1:])) for i in range(len(nums))]
 
 
 print(left_right_difference([1]))
