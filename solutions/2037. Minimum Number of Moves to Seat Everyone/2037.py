8051b1ea27496009f5cd0d7dd520cda54e1b8d8a
khasanjon <khasanjon.dev@gmail.com>
2024-03-10 17:34:00 +0500

    2037

--- solutions/2037. Minimum Number of Moves to Seat Everyone/2037.py
+++ solutions/2037. Minimum Number of Moves to Seat Everyone/2037.py
@@ -1,5 +1,12 @@
 def min_moves_to_seat(seats: list[int], students: list[int]) -> int:
-	pass
+    seats_sort = sorted(seats)
+    students_sort = sorted(students)
+    moves = 0
+    for i, student in enumerate(students_sort):
+        seat = seats_sort[i]
+        if student != seat:
+            moves += abs(student - seat)
+    return moves
 
 
-print(min_moves_to_seat([3, 1, 5], [2, 7, 4]))
+print(min_moves_to_seat([4, 1, 5, 9], [1, 3, 2, 6]))
