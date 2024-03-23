1b0acba0d2fa8a9aac64df208738cac95ddbd7f1
khasanjon <khasanjon.dev@gmail.com>
2024-03-23 07:55:14 +0500

    8

--- solutions/8. String to Integer (atoi)/8.py
+++ solutions/8. String to Integer (atoi)/8.py
@@ -1,18 +1,24 @@
 def my_atoi(s: str) -> int:
-    number = ''
-    char = ''
-    for num in s:
-        if num.isdigit():
-            if char:
-                number = ''
-                break
-            number += num
-        elif num == '-':
-            if not number:
-                number += '-'
-        elif num.isalpha():
-            char += num
-    return int(number) if number else 0
+    sign = 1
+    s = s.strip()
+    if not s:
+        return 0
+    if s[0] == '-':
+        sign = -1
+        s = s[1:]
+    elif s[0] == '+':
+        s = s[1:]
+    result = 0
+    for char in s:
+        if not char.isdigit():
+            break
+        digit = int(char)
+        result = result * 10 + digit
+        if sign == 1 and result >= 2 ** 31 - 1:
+            return 2 ** 31 - 1
+        elif sign == -1 and result >= 2 ** 31:
+            return -2 ** 31
+    return sign * result
 
 
-print(my_atoi('42'))
+print(my_atoi("+-12"))
