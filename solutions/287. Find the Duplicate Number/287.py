01e686de11451aa138e8fb373f1e7cb5c74d474e
khasanjon <khasanjon.dev@gmail.com>
2024-03-25 09:08:18 +0500

    287

--- solutions/287. Find the Duplicate Number/287.py
+++ solutions/287. Find the Duplicate Number/287.py
@@ -1,9 +1,9 @@
+from collections import Counter
+
+
 def find_duplicate(nums: list[int]) -> int:
-    i = 0
-    while i <= len(nums):
-        if nums.count(nums[i]) >= 2:
-            return nums[i]
-        i += 1
+    counter_nums = sorted(Counter(nums).items(), key=lambda item: -item[-1])
+    return counter_nums[0][0]
 
 
 print(find_duplicate([1, 3, 4, 2, 2]))
