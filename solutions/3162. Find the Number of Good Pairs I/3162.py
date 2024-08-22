7be5325fa3abb81db5c992b51f502053678fa6b2
khasanjon <khasanjon.dev@gmail.com>
2024-08-22 11:15:07 +0500

    3162. Find the Number of Good Pairs I

---
+++ solutions/3162. Find the Number of Good Pairs I/3162.py
@@ -0,0 +1,10 @@
+def number_of_pairs(nums1: list[int], nums2: list[int], k: int) -> int:
+    pairs = 0
+    for num1 in nums1:
+        for num2 in nums2:
+            if num1 % (num2 * k) == 0:
+                pairs += 1
+    return pairs
+
+
+print(number_of_pairs([1, 3, 4], [1, 3, 4], 1))
