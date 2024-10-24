65642cf5793b53810e546b8aa614e6f310ecb730
khasanjon <khasanjon.dev@gmail.com>
2024-10-24 18:02:25 +0500

    update some

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import base64
 
 
 def generate_qr_code(merchant_id, device_id):
-    base_url = f"https://beepul.uz/new/payment?qr=2&"
+    base_url = f"https://beepul.uz/actions/payment?qr=2&"
 
-    url = f"m={merchant_id}&ac.qurilma_id={device_id}"
+    url = f"m={merchant_id}&cr=860&ac.qurilma_id={device_id}&ac.merchant_id={merchant_id}"
     url_bytes = url.encode('utf-8')
     base64_encoded_url = base64.b64encode(url_bytes).decode('utf-8')
 
     return base_url + base64_encoded_url
 
 
-print(generate_qr_code(833, "Test qurilma 1"))
+qr = generate_qr_code(4092, "Sound Box")
 
 
 def decode(base64_encoded_url):
@@ -24,4 +24,5 @@ def decode(base64_encoded_url):
     print(decoded_url)
 
 
-decode("bT04MzMmY3I9ODYw")
+decode("bT1Tb3VuZCBCb3gmY3I9ODYwJmFjLnF1cmlsbWFfaWQ9U291bmQgQm94JmFjLm1lcmNoYW50X2lkPTQwOTI=")
+"https://beepul.uz/actions/payment?qr=2&bT1Tb3VuZCBCb3gmY3I9ODYwJmFjLnF1cmlsbWFfaWQ9U291bmQgQm94JmFjLm1lcmNoYW50X2lkPTQwOTI="
