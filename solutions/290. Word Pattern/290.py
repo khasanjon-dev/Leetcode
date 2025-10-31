a8c0b6fffae47e9c9bf7713a7662b2e03cb2d81a
khasanjon <khasanjon.dev@gmail.com>
2025-10-31 10:48:02 +0200

    290

--- solutions/290. Word Pattern/290.py
+++ solutions/290. Word Pattern/290.py
@@ -1,5 +1,17 @@
 def word_pattern(pattern: str, s: str) -> bool:
-    pass
+    s_list = s.split()
+    if len(s_list) != len(pattern):
+        return False
+    pattern_w = {}
+    w_pattern = {}
+    for p, w in zip(pattern, s_list):
+        if p in pattern_w and pattern_w[p] != w:
+            return False
+        if w in w_pattern and w_pattern[w] != p:
+            return False
+        pattern_w[p] = w
+        w_pattern[w] = p
+    return True
 
 
-print(word_pattern("abba", "dog cat cat dog"))
+print(word_pattern("abba", "dog dog dog dog"))
