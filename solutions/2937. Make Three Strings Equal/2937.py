f003f8be7f884422f1ba76fd51ae1ad5a370172f
khasanjon <khasanjon.dev@gmail.com>
2026-01-25 09:17:25 +0200

    2937. Make Three Strings Equal version 2

--- solutions/2937. Make Three Strings Equal/2937.py
+++ solutions/2937. Make Three Strings Equal/2937.py
@@ -1,13 +1,17 @@
 def find_minimum_operations(s1: str, s2: str, s3: str) -> int:
+    s1_len = len(s1)
+    s2_len = len(s2)
+    s3_len = len(s3)
+    min_len = min(s1_len, s2_len, s3_len)
     j = 0
-    for i, s in enumerate(zip(s1, s2, s3)):
-        if len(set(s)) == 1:
+    for i in range(min_len):
+        if s1[i] == s2[i] == s3[i]:
             j += 1
         elif i == 0:
             return -1
         else:
             break
-    return len(s1) + len(s2) + len(s3) - 3 * j
+    return s1_len + s2_len + s3_len - 3 * j
 
 
 print(find_minimum_operations("abc", "abb", "ab"))
