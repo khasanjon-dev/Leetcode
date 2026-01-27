1d905a308391971ebea075e0a03e22335be38fa1
khasanjon <khasanjon.dev@gmail.com>
2026-01-27 14:02:41 +0200

    2409. Count Days Spent Together

--- solutions/2409. Count Days Spent Together/2409.py
+++ solutions/2409. Count Days Spent Together/2409.py
@@ -1,22 +1,24 @@
 def count_days_together(
     arriveAlice: str, leaveAlice: str, arriveBob: str, leaveBob: str
 ) -> int:
-    ar_a_month, ar_a_day = map(int, arriveAlice.split("-"))  # ar => arrive, a => Alice
-    lv_a_month, lv_a_day = map(int, leaveAlice.split("-"))  # lv => leave, a => Alice
-    ar_b_month, ar_b_day = map(int, arriveBob.split("-"))  # ar => arrive, b => Bob
-    lv_b_month, lv_b_day = map(int, leaveBob.split("-"))  # lv => leave, b => Bob
     days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    b_range = range(ar_b_month, lv_b_month + 1)
-    a_range = range(ar_a_month, lv_a_month + 1)
-    print(b_range)
-    print(a_range)
-    if b_range in a_range:
-        print("good")
-    else:
-        print("bad")
+
+    def days_in_year(date: str) -> int:
+        month, day = map(int, date.split("-"))
+        return sum(days_in_month[: month - 1]) + day
+
+    arrive_alice = days_in_year(arriveAlice)
+    leave_alice = days_in_year(leaveAlice)
+    arrive_bob = days_in_year(arriveBob)
+    leave_bob = days_in_year(leaveBob)
+
+    start = max(arrive_alice, arrive_bob)
+    end = min(leave_alice, leave_bob)
+    return max(0, end - start - 1)
+
 
 print(
     count_days_together(
-        arriveAlice="01-01", leaveAlice="12-31", arriveBob="03-02", leaveBob="09-07"
+        arriveAlice="03-08", leaveAlice="11-28", arriveBob="01-01", leaveBob="12-31"
     )
 )
