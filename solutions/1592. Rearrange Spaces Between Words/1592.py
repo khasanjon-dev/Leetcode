590c027a7f5079857b089534adcf91aba2a6b25c
khasanjon <khasanjon.dev@gmail.com>
2026-01-30 22:11:52 +0200

    1592. Rearrange Spaces Between Words

---
+++ solutions/1592. Rearrange Spaces Between Words/1592.py
@@ -0,0 +1,17 @@
+def reorder_space(text: str) -> str:
+    spaces = text.count(" ")
+    words = text.split()
+
+    if len(words) == 1:
+        return words[0] + " " * spaces
+
+    gap, extra = divmod(spaces, len(words) - 1)
+    return (" " * gap).join(words) + " " * extra
+
+
+print(reorder_space(text="  this   is  a sentence "))
+"""
+Input: text = " practice   makes   perfect"
+Output: "practice   makes   perfect "
+Explanation: There are a total of 7 spaces and 3 words. 7 / (3-1) = 3 spaces plus 1 extra space. We place this extra space at the end of the string.
+"""
